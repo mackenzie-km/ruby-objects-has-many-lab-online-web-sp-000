ac007eea64c17bc577c663eee40657b1de024aef
Mackenzie Moore <macmoore@ucdavis.edu>
2019-03-04 21:11:54 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -13,5 +13,9 @@ class Author
     post.author = self 
     @posts << post 
   end 
-    
+  
+  def add_post_by_title(post_title)
+    post_title = Post.new(post_title)
+    post_title.author = self 
+  end
 end 
