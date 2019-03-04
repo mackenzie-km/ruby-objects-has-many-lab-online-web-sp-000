448677484e3077982f4ecf9340bb49d6c5f46aff
Mackenzie Moore <macmoore@ucdavis.edu>
2019-03-04 21:13:09 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,10 +1,15 @@
 class Post 
   attr_accessor :author, :title
+  
   @@all = [] 
+  
   def initialize(title)
     @title = title
+    self.all << self 
   end 
+  
   def self.all
     @@all 
   end 
+  
 end 
